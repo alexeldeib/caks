3c76ed24e13b3f378c32ce14e0de30f66617db61
Ace Eldeib <alexeldeib@gmail.com>
2019-12-16 01:18:40 -0800

    fix up test image + use in main

---
+++ images/copy.sh
@@ -0,0 +1,9 @@
+#!/usr/env/bin bash
+set -eux
+
+mkdir $GITHUB_WORKSPACE/bin
+cp /usr/local/kubebuilder/bin/* $GITHUB_WORKSPACE/bin
+cp /usr/local/bin/controller-gen $GITHUB_WORKSPACE/bin
+cp /usr/local/bin/kustomize $GITHUB_WORKSPACE/bin
+cp /usr/local/bin/kind $GITHUB_WORKSPACE/bin
+cp /usr/local/bin/kubectl $GITHUB_WORKSPACE/bin
